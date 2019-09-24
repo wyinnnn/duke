b4e6acb17a65d6620b233efbff4c3234a7c7d827
wyinnnn <e0323404@u.nus.edu>
2019-09-24 18:18:53 +0800

    level 3.1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
 import java.util.Scanner; //Importthe scanner class
+import task.Task;
 
-//class
 
+//JavaDoc should be in present tense
 
 
 public class Duke {
@@ -17,13 +18,8 @@ public class Duke {
         //do the thing
         Scanner myObj = new Scanner(System.in); //create a scanner object
 
-        //add to array
-        String[] myList = new String[100]; //declare an array of strings; allocate mmr for 100 objs of type myList
-        String[] Status = new String [100];
+        Task[] myTasks = new Task[100]; //declare an array of Tasks; allocate mmr for 100 objs of type Task
 
-        String Bye = "bye";
-        String tick ="✓";
-        String cross = "✗";
         int i = 0;
         int index;
 
@@ -34,24 +30,26 @@ public class Duke {
 
 
 
-            if (userInput.equals(Bye)) {
+            if (userInput.equals("bye")) {
                 System.out.println("Bye l0s3r c u again soon.");
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("Things in your list:");
                 for (int j = 0; j < i; j++) {
-                    System.out.println((j + 1) + ". " + "[" + Status[j] + "]" + myList[j]);
+                    System.out.println((j + 1) + ". " + "[" + myTasks[j].getStatusIcon() + "]" + myTasks[j].getDescription());
                 }
             } else if (Action[0].equals("done")){
-                int Number = Integer.parseInt(Action[1]); //PROBLEM
-                Status[Number - 1] = tick;
+                int Number = Integer.parseInt(Action[1]); //change string to INT
+
+                myTasks[Number - 1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + Status[Number -1] + "] " + myList[Number -1]);
+                System.out.println("[" + myTasks[Number -1].getStatusIcon() + "] " + myTasks[Number -1].getDescription());
             } else {
                 //add into array
-                myList[i] = userInput;
+
+                myTasks[i] = new Task(userInput);
                 System.out.println("added:" + userInput);
-                Status[i] = cross;
+
                 i++;
             }
         }
---
+++ src/main/java/task/Task.java
@@ -0,0 +1,40 @@
+package task;
+
+public class Task {
+    private String description;
+    private boolean isDone;
+    private String taskType;
+
+    public Task(String description){ //whenever user keys in a TASK
+        this.description = description; //THIS: differentiate Instance var from Local var
+        this.isDone = false; //cross
+    }
+
+
+    public String getStatusIcon() { //this is a METHOD for STATUS ICON
+        return (isDone ? "\u2713" : "\u2718"); //tick or cross
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getTaskType(){
+        return taskType;
+    }
+
+    public void setTaskType(String input){
+        this.taskType = input;
+    }
+
+    public void markAsDone(){
+        this.isDone = true; //tick
+    }
+
+
+}
+
+
+//Task t = new Task("read book"); // instantiate new task as 't', of type Task.
+//
+//t.markAsDone();
